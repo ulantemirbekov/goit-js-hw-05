eaa2c909b9a3919ddf464e7ee9107cf500d1fb70
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2020-11-22 18:42:56 +0200

    task-03 solution added

---
+++ task-03.js
@@ -0,0 +1,33 @@
+class Storage {
+    constructor(array) {
+        this.items = array;
+    };
+    getItems() {
+        return this.items;
+    };
+    addItem(item) {
+        this.items.push(item);
+    };
+    removeItem(item) {
+        const index = this.items.indexOf(item);
+        if (index >= 0) {
+            this.items.splice(index, 1);
+        };
+    };
+};
+
+const storage = new Storage([
+    'Нанитоиды',
+    'Пролонгер',
+    'Железные жупи',
+    'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
